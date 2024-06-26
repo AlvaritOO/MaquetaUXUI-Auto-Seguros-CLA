f84d0320629ca4e9fb0ae044b9785b1369863cf3
Fernando Gonzalez <fernandogonzalezgaete@gmail.com>
2024-06-26 11:39:15 -0400

    intento fix home

--- src/js/funciones2.js
+++ src/js/funciones2.js
@@ -52,4 +52,60 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-});
+}
+);
+const swiperdesktop = new Swiper('.swiper-desktop', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+  spaceBetween: 20,
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 150
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 150
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    1150: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    }
+
+  },
+
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-desktop',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar-desktop',
+  },
+}
+);
